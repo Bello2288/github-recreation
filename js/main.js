46069a6e4bf7682ba583263c84ed47dd0bcdbafa
Bello2288 <bellofattogregg@gmail.com>
2022-09-22 16:26:42 -0400

    fixed fetch

--- js/main.js
+++ js/main.js
@@ -1,33 +1,57 @@
-import repos from "./repos.js";
-import user from "./user.js";
-import orgs from "./orgs.js";
+// import repos from "./repos.js";
+// import user from "./user.js";
+// import orgs from "./orgs.js";
 
+const API_KEY = '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';
+
+(function() {
+    'use strict'
+// ========================================================================================================
+function generateHTML(data) {
+    const repoSource = document.getElementById("repo-template").innerHTML;
+    const repoTemplate = Handlebars.compile(repoSource);
+    const repoHtml = repoTemplate({repos:data[0]});
+
+    document.querySelector(".repository").insertAdjacentHTML("afterbegin", repoHtml);
 
 // ========================================================================================================
 
-const repoSource = document.getElementById("repo-template").innerHTML;
-const repoTemplate = Handlebars.compile(repoSource);
-const repoHtml = repoTemplate({repos});
+    const userSource = document.getElementById("profile-template").innerHTML;
+    const userTemplate = Handlebars.compile(userSource);
+    // console.log('user', user);
+    const userHtml = userTemplate({user:data[1]});
 
-document.querySelector(".repository").insertAdjacentHTML("afterbegin", repoHtml);
-
-// ========================================================================================================
-
-const userSource = document.getElementById("profile-template").innerHTML;
-const userTemplate = Handlebars.compile(userSource);
-console.log('user', user);
-const userHtml = userTemplate(user);
-
-document.querySelector(".user").insertAdjacentHTML("afterbegin", userHtml);
+    document.querySelector(".user").insertAdjacentHTML("afterbegin", userHtml);
 
 // ========================================================================================================
 
-const orgsSource = document.getElementById("orgs-template").innerHTML;
-const orgsTemplate = Handlebars.compile(orgsSource);
-const orgsHtml = orgsTemplate({orgs});
+    const orgsSource = document.getElementById("orgs-template").innerHTML;
+    const orgsTemplate = Handlebars.compile(orgsSource);
+    const orgsHtml = orgsTemplate({orgs:data[2]});
 
-document.querySelector(".orgs").insertAdjacentHTML("afterbegin", orgsHtml);
+    document.querySelector(".orgs").insertAdjacentHTML("afterbegin", orgsHtml);
+}
 
+Promise.all([
+    fetch(`https://api.github.com/users/Bello2288/repos`),
+    fetch(`https://api.github.com/users/Bello2288`),
+    fetch(`https://api.github.com/users/Bello2288/orgs`),
+])
+.then(function(responses) {
+    console.log('response', responses);
+    return Promise.all(responses.map(function (response) {
+        return response.json();
+    }));
+})
+.then(function(json) {
+    console.log('json', json);
+    generateHTML(json);
+})
+.catch(function(error) {
+    console.log('Error', error);
+});
+
+})();
 
 
 // ==========================================================================================================
@@ -39,10 +63,10 @@ document.querySelector(".orgs").insertAdjacentHTML("afterbegin", orgsHtml);
 //     fetch(`https://api.github.com/users/Bello2288`),
 //     fetch(`https://api.github.com/users/Bello2288/orgs`),
 // ])
-// .then(function(response) {
+// .then(function(responses) {
 //     console.log('response', responses);
 //     return Promise.all(responses.map(function (response) {
-//         return resoonse.json();
+//         return response.json();
 //     }));
 // })
 // .then(function(json) {
